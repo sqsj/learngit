a45ab98abf63c93ce6b06ea55cf03ab1397f7d10
zhouzhong <zhouzhonghappy@163.com>
2017-11-27 11:06:21 +0800

    first commit

---
+++ app/src/main/java/learn/git/com/mygitlearn/MainActivity.java
@@ -0,0 +1,13 @@
+package learn.git.com.mygitlearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
