8f3e485aeb2c31582f4441975313664604285f1e
zhouzhong <zhouzhonghappy@163.com>
2017-12-11 14:36:19 +0800

    添加MyCountryActivity

--- app/src/main/java/learn/git/com/mygitlearn/MyCountryActivity.java
+++ app/src/main/java/learn/git/com/mygitlearn/MyCountryActivity.java
@@ -1,8 +1,16 @@
 package learn.git.com.mygitlearn;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by zhouzhong on 2017/11/27.
  */
 
-public class MyCountryActivity {
+public class MyCountryActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
